c4f91a566e79e08e4b13a01690ebe836cf66ba3d
gpika <gpika@emphasoft.com>
2021-03-12 18:41:20 +0300

    trying this with button

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ function App() {
 
   const inputRef = useRef();
 
-  useEffect(() => {
-    const button = document.createElement("button");
-    button.addEventListener('click', () => inputRef.current.focus());
-    button.click();
-  }, [])
+  // useEffect(() => {
+  //   const button = document.createElement("button");
+  //   button.addEventListener('click', () => inputRef.current.focus());
+  //   button.click();
+  // }, [])
 
   return (
     <div className="App">
       <form>
         <input ref={inputRef} ></input>
       </form>
+      <button onClick={() => inputRef.current.focus()}>нажми меня</button>
     </div>
   );
 }
